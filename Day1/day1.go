de5ff153964792708cd30e57e153d8a4b123583d
aagoldingay <aarongoldingay@hotmail.com>
2017-12-04 23:22:53 +0000

    Day1 part 1 solution, part 2 setup

--- Day1/day1.go
+++ Day1/day1.go
@@ -1,8 +1,28 @@
-package Day1
+package main
 
-import (
-)
+import "fmt"
 
-func ReviewSequence(s string) string {
-  return s;
+func ReviewSequenceP1(s string) int {
+  lastNum, result := 0, 0
+  for i, c := range s {
+    if int(c)%48 == lastNum {
+      result += lastNum
+    }
+    lastNum = (int(c) % 48)
+    if ((i == len(s)-1) && ((int(c)%48) == int(s[0])%48)) {
+      result += int(s[0])%48
+    }
+  }
+  return result
+}
+
+func ReviewSequenceP2(s string) int {
+  half, result := len(s)/2, 0
+  //figure out mod
+  result = half
+  return result
+}
+
+func main() {
+  fmt.Println("placeholder")
 }
--- Day1/day1_test.go
+++ Day1/day1_test.go
@@ -1,12 +1,40 @@
-package Day1
+package main
 
 import (
+	"fmt"
 	"testing"
 )
 
-func Test_ReviewSequence(t *testing.T) {
-	s := ReviewSequence("hello")
-	if s != "hello" {
-		t.Error("output not as expected")
+var testsp1 = []string {"1122", "1234", "1111", "91212129"}
+var resultsp1 = []int {3, 0, 4, 9}
+
+var testsp2 = []string {"1212", "1221", "123425", "123123", "12131415"}
+var resultsp2 = []int {6, 0, 4, 12, 4}
+
+func Test_ReviewSequenceP1(t *testing.T) {
+	fmt.Println("TEST PART 1")
+	for i, s := range testsp1 {
+		fmt.Println("testing : ", s)
+		n := ReviewSequenceP1(s)
+		fmt.Println("result :", n)
+		if n != resultsp1[i] {
+			t.Errorf("ERR - expected: %d, got: %d", resultsp1[i], n)
+		}
 	}
+}
+
+func Test_ReviewSequenceP2(t *testing.T) {
+	fmt.Println("TEST PART 2")
+	n := ReviewSequenceP2("11") 
+	if n != 1 {
+		t.Errorf("ERR - expected: %d, got: %d", 1, n)
+	}
+	/*for i, s := range testsp2 {
+		fmt.Println("testing : ", s)
+		n := ReviewSequenceP2(s)
+		fmt.Println("result : ", n)
+		if n != resultsp2[i] {
+			t.Errorf("ERR - expected: %d, got %d", resultsp2[i], n)
+		}
+	}*/
 }
